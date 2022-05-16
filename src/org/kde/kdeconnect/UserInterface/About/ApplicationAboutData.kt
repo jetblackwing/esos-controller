70b96c7a7f72cc3985ae2afe69d3685da28ebd27
amal <amaljk80@gmail.com>
2022-05-16 11:55:56 +0530

    Initial Commit to Extended Support Operating System (ESOS) Controller

--- src/org/kde/kdeconnect/UserInterface/About/ApplicationAboutData.kt
+++ src/org/kde/kdeconnect/UserInterface/About/ApplicationAboutData.kt
@@ -18,9 +18,10 @@ fun getApplicationAboutData(context: Context): AboutData {
                               context.getString(R.string.report_bug_url), context.getString(R.string.website_url), context.getString(R.string.source_code_url), context.getString(R.string.donate_url),
                               R.string.everyone_else)
 
-    aboutData.authors += AboutPerson("Albert Vaca Cintora", R.string.maintainer_and_developer, "albertvaka+kde@gmail.com")
-    aboutData.authors += AboutPerson("Aleix Pol", R.string.developer, "aleixpol@kde.org")
-    aboutData.authors += AboutPerson("Inoki Shaw", R.string.apple_support, "veyx.shaw@gmail.com")
+    aboutData.authors += AboutPerson("Amal J Krishnan", R.string.maintainer_and_developer, "amaljk80@gmail.com")
+    aboutData.authors += AboutPerson("Abhinu Mani", R.string.developer, "abhinu62@gmail.com")
+    aboutData.authors += AboutPerson("Sreekanth R", R.string.apple_support, "nighcoder.c@gmail.com")
+    /**
     aboutData.authors += AboutPerson("Matthijs Tijink", R.string.developer, "matthijstijink@gmail.com")
     aboutData.authors += AboutPerson("Nicolas Fella", R.string.developer, "nicolas.fella@gmx.de")
     aboutData.authors += AboutPerson("Philip Cohn-Cort", R.string.developer, "cliabhach@gmail.com")
@@ -37,6 +38,6 @@ fun getApplicationAboutData(context: Context): AboutData {
     aboutData.authors += AboutPerson("Maxim Leshchenko", R.string.maxim_leshchenko_task, "cnmaks90@gmail.com")
     aboutData.authors += AboutPerson("Holger Kaelberer", R.string.holger_kaelberer_task, "holger.k@elberer.de")
     aboutData.authors += AboutPerson("Saikrishna Arcot", R.string.saikrishna_arcot_task, "saiarcot895@gmail.com")
-
+    */
     return aboutData
 }
